72b48e87d1adfb057cd6f01650f9fe568ecfb695
Abbos <savrjonov04@gmail.com>
2025-09-30 21:34:36 +0500

    fix(Header): remove breadcrumb

--- src/app/_widgets/Header/Header.tsx
+++ src/app/_widgets/Header/Header.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import { Bell, FileText, Inbox } from "lucide-react";
-import { usePathname } from "next/navigation";
 import React from "react";
-import { BreadcrumbWidget } from "../Breadcrumb";
 
 export function Header() {
-  const pathname = usePathname();
-
   return (
     <div className="h-[73px] flex items-center justify-between px-7 py-5 border-b-1">
-      <BreadcrumbWidget pathName={pathname} />
+      <h1 className="text-2xl font-bold text-[var(--color-gray-2)]">
+        Dashboard
+      </h1>
       <ul className="flex gap-3">
         <li className="cursor-pointer">
           <Inbox className="size-7" />
